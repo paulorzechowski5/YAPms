a9e04aabb48949420f312d9da595df3e3d4ec61a
Jacob Enders <jacobenders1@gmail.com>
2019-01-31 00:28:42 -0500

    removed google ads

--- index.php
+++ index.php
@@ -26,26 +26,15 @@
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-132710089-1"></script>
 	<script>
 		var host = window.location.hostname;
-		if(host !== 'localhost') {
-			console.log(host);
-			window.dataLayer = window.dataLayer || [];
-			function gtag(){dataLayer.push(arguments);}
-			gtag('js', new Date());
-			gtag('config', 'UA-132710089-1');
-		}
+		console.log(host);
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'UA-132710089-1');
 	</script>
 	
-	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	<script>
-		(adsbygoogle = window.adsbygoogle || []).push({
-			google_ad_client: "ca-pub-1660456925957249",
-			enable_page_level_ads: true
-		});	
-	</script>
-
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
 	<link rel="stylesheet" type="text/css" href="style.css">
-
 </head>
 
 <body>
