5e38af02a48819134c36771fb6818b1e83fa4614
Jacob Enders <jacobenders1@gmail.com>
2019-02-22 19:23:22 -0500

    updated service worker

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,4 @@
-var currentCache = 'v0.8.4';
+var currentCache = 'v0.8.5';
 
 var states = [];
 var lands = [];
--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-var currentCache = 'v0.8.4';
+var currentCache = 'v0.8.5';
 
 function swLog(message) {
 	console.log('SW' + currentCache + ': ' + message);
